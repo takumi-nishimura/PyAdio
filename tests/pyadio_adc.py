eb99f8415a0106df98d9c70fe4dcf1503a40da5a
takumi-nishimura <clp13218@nitech.jp>
2025-03-12 10:30:50 +0900

    不要なインポートとソケット送信処理を削除

--- tests/pyadio_adc.py
+++ tests/pyadio_adc.py
@@ -1,6 +1,4 @@
 import logging
-import pickle
-import socket
 
 from pyadio import PyAdio
 
@@ -9,8 +7,6 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-
-    plot_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     REQUEST_DATA_NUM = 6
     pyadio = PyAdio()
@@ -30,8 +26,9 @@ def main():
     try:
         x = 0
         while True:
-            recv_data_dict = {}
+            x += 1
 
+            recv_data_dict = {}
             for _ in range(REQUEST_DATA_NUM):
                 ch, data = pyadio.adc.get_buffer_data()
                 if ch is not None:
@@ -43,12 +40,6 @@ def main():
                         continue
 
                     recv_data_dict[ch] = data
-
-            x += 1
-            send_data = {"x": x}
-            for ch, data in recv_data_dict.items():
-                send_data[f"y{ch+1}"] = data
-            plot_sock.sendto(pickle.dumps(send_data), ("localhost", 4000))
 
     except Exception as e:
         logger.error(e)
